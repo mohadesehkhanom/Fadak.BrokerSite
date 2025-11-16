7718a7175d4897e209aaa4d467019ec9c38580f1
Amiri267 <m.amiri@gmail.com>
2025-11-16 23:13:58 +0330

    fix error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,21 @@
 import SectionNavbarMenu from "@/section/SectionHeader/SectionNavbarMenu";
-import SectionFooter from "@/section/SectionFooter/SectionFooter";
-import Link from "next/link";
 
 export default function NotFound() {
     return (
 
         <>
             <SectionNavbarMenu/>
-
-            <section className="error-page d-center text-center section bg-fixed"
-                     data-background="assets/images/element/common-bg.png">
-                <div className="container">
-                    <div className="row justify-content-center">
+            <section className="error-page d-center text-center section bg-fixed ">
+                <div className="container h-100">
+                    <div className="row justify-content-center align-items-center h-100">
                         <div className="col-12 col-lg-9 col-xxl-8">
-                            <div className="error-page__thumb wow fadeInDown" data-wow-duration="0.8s">
-                                <img src="/assets/images/review/error-thumb.png" alt="images"/>
+                            <div className="error-page__thumb">
+                                <img src="/assets/images/review/404.jpg" alt="images"/>
                             </div>
                         </div>
                         <div className="col-12 col-md-8 col-xxl-6">
                             <div className="section__content mt-5">
-                                <h2 className="section__content-title wow fadeInUp" data-wow-duration="0.8s">
+                                <h2 className="section__content-title text-light">
                                     صفحه مورد نظر یافت نشد
                                 </h2>
                                 {/*<Link href="/" className="btn fadak-btn">*/}
