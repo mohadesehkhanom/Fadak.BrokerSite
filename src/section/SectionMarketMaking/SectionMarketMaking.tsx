928a6421f6f22c14480f958d846e901e5cac9bcd
Amiri267 <m.amiri@gmail.com>
2025-12-19 18:59:51 +0330

    fix contact page

---
+++ src/section/SectionMarketMaking/SectionMarketMaking.tsx
@@ -0,0 +1,175 @@
+'use client'
+import "./SectionMarketMaking.css"
+
+export default function SectionMarketMaking() {
+
+
+
+    return (
+        < >
+            <section className="header-derivative">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 d-flex flex-column align-items-center justify-content-center" style={{minHeight: "500px"}}>
+                            <h4>صندوق بازارگردانی فدک</h4>
+                            <div className="mt-4">
+                                <a href="" target="_blank" className="btn fadak-button me-3">شروع سرمایه گذاری</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="p-5 bg-white">
+                <div className="container my-5">
+                    {/*<h4 className="section-title w-100 text-center">سامانه معاملاتی در بازار مشتقه امکس</h4>*/}
+
+                    <div className="row">
+                        <div className="col-lg-8 mt-0 mx-auto">
+                            <div className="card p-4" dir="rtl">
+                                <div className="card-body">
+
+                                    <h5 className="card-title mb-4">اهمیت صندوق بازارگردانی</h5>
+
+                                    <p className="card-text mb-4">
+                                        یکی از مهم‌ترین مزایای بازار سرمایه، امکان تبدیل سریع دارایی به وجه نقد است؛ اما در شرایط هیجانی بازار، شکل‌گیری صف‌های طولانی خرید یا فروش می‌تواند نقدشوندگی سهم را کاهش دهد و به اعتماد سرمایه‌گذاران آسیب بزند.
+                                        صندوق‌های بازارگردانی با ایجاد تعادل میان عرضه و تقاضا، این ریسک را کنترل کرده و کمک می‌کنند همواره خریدار و فروشنده در بازار حضور داشته باشند. این فرآیند موجب ثبات معاملات، کاهش نوسانات غیرمنطقی و افزایش اطمینان سرمایه‌گذاران به سهم شرکت می‌شود.
+                                    </p>
+
+                                    <p className="card-text mb-4">
+                                        بازارگردانی حرفه‌ای، علاوه بر حفظ نقدشوندگی، به خوشنامی سهم و تقویت تصویر شرکت در بازار سرمایه کمک می‌کند و باعث می‌شود سهام شرکت به‌عنوان گزینه‌ای قابل اعتماد نزد فعالان بازار شناخته شود. کارگزاری فدک این خدمت را با رویکردی تخصصی و مطابق با ضوابط بازار سرمایه ارائه می‌دهد.
+                                    </p>
+
+                                    {/*<a href="https://fadak.tsetab.ir" className="btn fadak-button float-end">ورود به سامانه امکس</a>*/}
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-4 d-flex align-items-center justify-content-center">
+                            <img className="d-sm-none d-md-flex mb-sm-2 mb-lg-0 rounded" src="https://picsum.photos/420/320"/>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <section id="services" className="p-5 fadak-bg-cream">
+                <div className="container my-5">
+                    <h4 className="section-title text-center w-100 my-0 py-3 border-0">جهت دریافت اطلاعات تماس حاصل فرمایید</h4>
+                    {/*<p className="mt-2">*/}
+                    {/*    تیم مشتقه کارگزاری فدک مجموعه‌ای از خدمات تحلیلی، آموزشی و اجرایی را برای*/}
+                    {/*    معامله‌گران حرفه‌ای و سرمایه‌گذاران حقوقی فراهم کرده است تا با اطمینان و سرعت*/}
+                    {/*    بیشتر در بازار اختیار معامله و آتی فعالیت کنند.*/}
+                    {/*</p>*/}
+
+
+
+
+                    <div className="row justify-content-center align-items-center text-center py-2 rounded-2" style={{backgroundColor: "rgb(0 61 106)"}}>
+
+                        <div className="col-md-4">
+                            <div className="contact-item text-white">
+                                <a href="tel:02163479410" className="text-white text-decoration-none d-block gap-2 d-flex align-items-center justify-content-center">
+                                    <span>02163479410</span>
+                                    <i className="bi bi-telephone-fill"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="contact-item text-white">
+                                <a href="mailto:Fund@fadakbrokerage.ir" className="text-white text-decoration-none d-block gap-2 d-flex align-items-center justify-content-center">
+                                    <span>Fund@fadakbrokerage.ir</span>
+                                    <i className="bi bi-envelope-fill"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+
+            <section className="p-5 bg-white">
+                <div className="container my-5">
+                    {/*<h4 className="section-title w-100 text-center">سامانه معاملاتی در بازار مشتقه امکس</h4>*/}
+
+                    <div className="row">
+                        <div className="col-lg-4 d-flex align-items-center justify-content-center">
+                            <img className="d-sm-none d-md-flex mb-sm-2 mb-lg-0 rounded" src="https://picsum.photos/420/320"/>
+                        </div>
+                        <div className="col-lg-8 mt-0 mx-auto">
+                            <div className="card p-4" dir="rtl">
+                                <div className="card-body">
+
+                                    <h5 className="card-title mb-4">صندوق بازارگردانی </h5>
+
+                                    <p className="card-text mb-4">
+                                        صندوق بازارگردانی ابزاری است که با هدف حفظ تعادل معاملات یک سهم یا گروهی از سهام فعالیت می‌کند. این صندوق با حضور فعال در بازار، از ایجاد نوسانات شدید قیمتی و صف‌های سنگین خرید و فروش جلوگیری کرده و باعث می‌شود معاملات روان‌تر انجام شوند. نتیجه این فعالیت، افزایش نقدشوندگی و کاهش امکان دست‌کاری قیمت سهام است.
+                                    </p>
+
+                                    <p className="card-text mb-4">
+                                        کارگزاری فدک از طریق خدمات بازارگردانی، به متعادل ماندن بازار و ارتقای کیفیت معاملات کمک می‌کند.
+                                    </p>
+
+                                    {/*<a href="https://fadak.tsetab.ir" className="btn fadak-button float-end">ورود به سامانه امکس</a>*/}
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <section id="onboarding" className="onboarding-section fadak-bg-cream p-5">
+                <div className="container my-5">
+                    <h4 className="section-title w-100 text-center">مزایای صندوق بازارگردانی</h4>
+
+                    <div className="row mt-4">
+                        <div className="col-md-3 mb-4">
+                            <div className="step-card">
+                                <div className="step-number">۱</div>
+                                <h6 className="step-title">افزایش نقدشوندگی و کاهش صف‌های خرید و فروش</h6>
+                            </div>
+                        </div>
+
+                        <div className="col-md-3 mb-4">
+                            <div className="step-card">
+                                <div className="step-number">۲</div>
+                                <h6 className="step-title">جلوگیری از نوسانات شدید و غیرمنطقی قیمت</h6>
+                            </div>
+                        </div>
+
+                        <div className="col-md-3 mb-4">
+                            <div className="step-card">
+                                <div className="step-number">۳</div>
+                                <h6 className="step-title">حفظ ارزش ذاتی سهام و کاهش رفتارهای سودجویانه</h6>
+                            </div>
+                        </div>
+
+                        <div className="col-md-3 mb-4">
+                            <div className="step-card">
+                                <div className="step-number">۴</div>
+                                <h6 className="step-title"> ایجاد تعادل و امنیت بیشتر در معاملات</h6>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*<div className="row mt-5">*/}
+                    {/*    <div className="col-12 text-center">*/}
+                    {/*        <p className="lead mb-4">*/}
+                    {/*            سریع‌تر، دقیق‌تر، حرفه‌ای‌تر؛ همه‌چیز برای یک معامله‌گر واقعی آماده است.*/}
+                    {/*        </p>*/}
+                    {/*        <a href="https://fadakregister.irbrokersite.ir/" target="_blank" className="btn fadak-button btn-lg">همین حالا به جمع معامله‌گران فدک بپیوندید</a>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+
+                </div>
+            </section>
+
+
+        </>
+    );
+}
