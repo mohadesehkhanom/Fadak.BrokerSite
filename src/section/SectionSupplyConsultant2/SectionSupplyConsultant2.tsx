457d0b431a21bdc21dc4546b9a731711af6a517e
Amiri267 <m.amiri@gmail.com>
2025-12-26 18:26:25 +0330

    fix contact page

---
+++ src/section/SectionSupplyConsultant2/SectionSupplyConsultant2.tsx
@@ -0,0 +1,97 @@
+import "./SectionSupplyConsultant2.css"
+import React from "react";
+
+export default function SectionSupplyConsultant2() {
+    return (
+        < >
+            <div className="content-section pt-5 pb-0">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6">
+                            <div className="image-container">
+                                <img
+                                    src="/assets/images/background/SectionSupplyConsultant.png"
+                                    width="600" height="400"
+                                    alt="مشاوره مالی و سرمایه‌گذاری"/>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 mb-4 mb-lg-0 mt-3 mt-lg-0">
+                            <h4>مشاوره پذیرش شرکت‌ها در بازار سرمایه</h4>
+                            <p className="mt-3">تأمین مالی پایدار و کم‌هزینه، کلید توسعه و افزایش ظرفیت‌های عملیاتی بنگاه‌های اقتصادی با ورود به
+                                بازار سرمایه.</p>
+
+                            <div className="row justify-content-center align-items-center text-center my-3 py-2 rounded-2" style={{backgroundColor: "#003d6a"}}>
+
+                                <div className="col-md-6">
+                                    <div className="contact-item text-white">
+                                        <a href="tel:02163479410" className="text-white text-decoration-none d-block gap-2 d-flex align-items-center justify-content-center">
+                                            <span>02163479410</span>
+                                            <i className="bi bi-telephone-fill"></i>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-6">
+                                    <div className="contact-item text-white">
+                                        <a href="mailto:marketentry@fadakbrokerage.ir" className="text-white text-decoration-none d-block gap-2 d-flex align-items-center justify-content-center">
+                                            <span>marketentry@fadakbrokerage.ir</span>
+                                            <i className="bi bi-envelope-fill"></i>
+                                        </a>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="services-list d-flex flex-column gap-5">
+                        <h4>مهم‌ترین خدمات مشاوره عرضه کارگزاری فدک</h4>
+                        <ul className="w-100">
+                            <li><i className="fas fa-check-circle"></i>بررسی دقیق وضعیت مالی، عملیاتی و ساختاری شرکت </li>
+                            <li><i className="fas fa-check-circle"></i>تحلیل میزان آمادگی برای پذیرش در بازارهای مختلف بورس و فرابورس </li>
+                            <li><i className="fas fa-check-circle"></i>شناسایی نقاط قابل بهبود و ارائه نقشه راه پذیرش </li>
+                            <li><i className="fas fa-check-circle"></i>تشریح الزامات، فرآیندها، آیین‌نامه‌ها و مقررات سازمان بورس </li>
+                            <li><i className="fas fa-check-circle"></i>ارائه مشاوره مستمر به سهامداران عمده، مدیران و مسئولان شرکت </li>
+                            <li><i className="fas fa-check-circle"></i>رفع ابهامات در تمامی مراحل تعامل با بورس/فرابورس </li>
+                            <li><i className="fas fa-check-circle"></i>تدوین امیدنامه ناشر و تهیه کلیه مدارک و مستندات مورد نیاز </li>
+                            <li><i className="fas fa-check-circle"></i>تهیه طرح تجاری (Business Plan) در صورت ضرورت پذیرش در بورس </li>
+                            <li><i className="fas fa-check-circle"></i>ارائه گزارش‌های تحلیلی تکمیلی برای بهبود جایگاه شرکت در فرآیند پذیرش  </li>
+                            <li><i className="fas fa-check-circle"></i>بررسی اساسنامه شرکت و ارائه پیشنهادهای اصلاحی </li>
+                            <li><i className="fas fa-check-circle"></i>انطباق اساسنامه با نسخه مورد تأیید سازمان بورس </li>
+                            <li><i className="fas fa-check-circle"></i>تحلیل ساختار سهامداری و پیشنهاد بهینه‌سازی‌های لازم </li>
+                            <li><i className="fas fa-check-circle"></i>هماهنگی جلسات مشترک میان مدیران شرکت و نهادهای پذیرش </li>
+                            <li><i className="fas fa-check-circle"></i>همراهی در بازدیدهای کارشناسی از سوی بورس/فرابورس </li>
+                            <li><i className="fas fa-check-circle"></i>نظارت بر رعایت تمامی مقررات در طول فرآیند پذیرش </li>
+                            <li><i className="fas fa-check-circle"></i>ارائه گزارش‌ها و دفاع از پرونده شرکت در جلسات رسمی </li>
+                            <li><i className="fas fa-check-circle"></i>پیگیری رفع ابهامات و تکمیل اطلاعات مورد درخواست هیأت پذیرش </li>
+                            <li><i className="fas fa-check-circle"></i>هماهنگی بازدید سرمایه‌گذاران و تحلیلگران از شرکت </li>
+                            <li><i className="fas fa-check-circle"></i>حضور در جلسات مذاکره مربوط به تعیین قیمت پایه سهام </li>
+                            <li><i className="fas fa-check-circle"></i>ارائه پیشنهادهای کارشناسی برای تحلیل و تعیین قیمت مناسب </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+                <div className="container-fluid mt-5">
+                    <div className="row">
+                        <div className="col-12 px-0">
+                            <section className="fadak-bg-navy p-5 text-center">
+                                <div className="container d-flex flex-column gap-5">
+                                    <h4 className="text-white">همراهی تا تحقق پذیرش</h4>
+                                    <p className="text-white text-center">
+                                        کارگزاری فدک با رویکردی مبتنی بر دقت، شفافیت و مسئولیت‌پذیری، شرکت‌ها را از نخستین مرحله بررسی
+                                        تا تصویب نهایی در بورس یا فرابورس همراهی می‌کند. هدف ما، تسهیل مسیر ورود شرکت‌ها به بازار سرمایه
+                                        و فراهم‌سازی بسترهای لازم برای توسعه پایدار و جذب سرمایه‌گذاران است.
+                                    </p>
+                                    <a  href="tel:02163479410" className="fadak-button">درخواست مشاوره تخصصی</a>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
