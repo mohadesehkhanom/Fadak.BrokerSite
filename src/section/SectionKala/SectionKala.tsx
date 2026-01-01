b47e539d5b651d07b9d16c3cfcacfbb6d932ce38
Amiri267 <m.amiri@gmail.com>
2026-01-01 22:52:11 +0330

    fix contact page

--- src/section/SectionKala/SectionKala.tsx
+++ src/section/SectionKala/SectionKala.tsx
@@ -3,6 +3,7 @@ import "./SectionKala.css"
 export default function SectionKala() {
     return (
         < >
+
             <section className="section-kala">
                 <div className="container">
                     <div className="row">
@@ -17,12 +18,12 @@ export default function SectionKala() {
             <section className="commodity-section">
                 <div className="container">
                     <div className="row align-items-center">
-                        <div className="col-lg-6 text-center">
+                        <div className="col-lg-5 text-center">
                             <img
                                 src="https://picsum.photos/600/400"
                                 className="metal-img rounded-4"/>
                         </div>
-                        <div className="col-lg-6 mb-4 mb-lg-0">
+                        <div className="col-lg-7 mb-4 mb-lg-0 mt-5 mt-lg-0">
                             <h4 className="fw-bold mb-4">معرفی بورس کالا</h4>
                             <p className=" lh-lg mb-4 text-justify">
                                 شرکت بورس کالای ایران در آذر ۱۳۸۵ با ادغام بورس فلزات و بورس کالای کشاورزی تشکیل شد و از
@@ -37,9 +38,18 @@ export default function SectionKala() {
                                 استفاده از زیرساخت‌های نوین و سیستم‌های معاملاتی پیشرفته، توانسته جایگاهی متمایز در میان
                                 کارگزاران بورس کالا به دست آورد.
                             </p>
-                            <a href="#" className="custom-btn-navy float-end">
-                                <i className="bi bi-file-earmark-text-fill ms-2"></i> کدگیری و پذیرش
-                            </a>
+                            <div className="d-flex flex-column flex-lg-row align-items-center justify-content-between gap-2">
+
+                                    <a href="/assets/doc/kala_haqiqi.pdf" className="custom-btn-navy text-center w-100 ">
+                                        <span className="w-100 text-center">فرم مشتریان حقیقی</span>
+                                    </a>
+                                    <a href="/assets/doc/BusinessClients.pdf" className="custom-btn-navy text-center w-100">
+                                        <span className="w-100 text-center">فرم مشتریان حقوقی</span>
+                                    </a>
+                                    <a href="/assets/doc/Required_Documents_For_Legal_Entity_Trading_Code_Registration.pdf" className="custom-btn-navy text-center w-100 ">
+                                        <span className="w-100 text-center">مدارک مشتریان حقوقی</span>
+                                    </a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -50,7 +60,7 @@ export default function SectionKala() {
                     <div className="row justify-content-center">
                         <div className="col-md-8">
                             <h4 className="vision-title">آمار و عملکرد کارگزاری فدک</h4>
-                            <p className="lead mt-3">
+                            <p className="lead mt-3 text-center">
                                 ما در کارگزاری فدک با تکیه بر دانش متخصصان و تجربه چندین ساله، مسیری امن و شفاف را برای
                                 معاملات کلیه کالاها، فرآورده‌های نفتی و انرژی شما در بازار سرمایه ایران فراهم کرده‌ایم.
                             </p>
@@ -63,19 +73,19 @@ export default function SectionKala() {
                 <div className="container">
                     <div className="text-center mb-5">
                         <h4 className="fw-bold">خدمات کارگزاری فدک در بورس کالا</h4>
-                        <p className="mt-3">ارائه خدمات جامع برای ورود و فعالیت موفق شما در بازارهای کالایی</p>
+                        <p className="mt-3 text-center">ارائه خدمات جامع برای ورود و فعالیت موفق شما در بازارهای کالایی</p>
                     </div>
 
 
-                    <div className="row ">
+                    <div className="row g-3 gy-lg-0 gx-lg-3">
 
-                        <div className="col-md-4">
+                        <div className="col-12 col-lg-3">
                             <div className="card h-100 fadak-bg-cream rounded-4">
                                 <div className="card-body text-center d-flex flex-column justify-content-around">
 
                                     <div className="mb-3 d-flex flex-column gap-3">
                                         <i className="bi bi-person-badge fs-1"></i>
-                                        <h5 className="fw-bold">دریافت کد بورس</h5>
+                                        <h5 className="fw-bold">دریافت کد بورسی</h5>
                                     </div>
 
                                     <p className="fadak-color-navy mt-3">
@@ -84,13 +94,13 @@ export default function SectionKala() {
                                         کدگیری برای مشتریان حقیقی و حقوقی.
                                     </p>
 
-                                    <a href="/assets/doc/Required_Documents_For_Legal_Entity_Trading_Code_Registration.pdf" target="_blank" className="d-block mt-5 fadak-color-navy fadak-btn-outline">مدارک مورد نیاز</a>
+                                    <a href="https://fadak.ebgo.ir/Login" target="_blank" className="d-block mt-5 fadak-color-navy fadak-btn-outline">اخذ کد بورس کالا</a>
 
                                 </div>
                             </div>
                         </div>
 
-                        <div className="col-md-4">
+                        <div className="col-12 col-lg-6 ">
                             <div className="card h-100 fadak-bg-cream rounded-4">
                                 <div className="card-body text-center d-flex flex-column justify-content-around">
 
@@ -104,13 +114,17 @@ export default function SectionKala() {
                                         مشاوره تخصصی برای عرضه عادلانه کالا.
                                     </p>
 
-                                    <a href="#" className="d-block mt-5 fadak-color-navy fadak-btn-outline">(لینک به فرم‌ها)</a>
+                                    <div className="d-flex flex-column flex-md-row justify-content-between gap-2 mt-auto">
+                                        <a href="https://ime.co.ir/Compliance_Documents_physical_market.html" className=" w-100 text-center fadak-color-navy fadak-btn-outline">بازار فیزیکی</a>
+                                        <a href="https://ime.co.ir/Compliance_Documents_subsidiary_market.html" className=" w-100 text-center fadak-color-navy fadak-btn-outline">بازار فرعی</a>
+                                        <a href="https://ime.co.ir/Compliance_Documents_Differential_market.html" className=" w-100 text-center fadak-color-navy fadak-btn-outline">بازار مشتقه</a>
+                                    </div>
 
                                 </div>
                             </div>
                         </div>
 
-                        <div className="col-md-4">
+                        <div className="col-12 col-lg-3">
                             <div className="card h-100 fadak-bg-cream rounded-4">
                                 <div className="card-body text-center d-flex flex-column justify-content-around">
 
@@ -124,125 +138,20 @@ export default function SectionKala() {
                                         گزارشات روزانه و هفتگی بازار.
                                     </p>
 
-                                    <a href="#" className="d-block mt-5 fadak-color-navy fadak-btn-outline">(نمای بازار و کانال تحلیلی)</a>
-
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-
-                </div>
-            </section>
-
-            <section className="commodity-section">
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-lg-6 mb-4 mb-lg-0">
-                            <h4 className="fw-bold mb-4">درباره بورس انرژی</h4>
-                            <p className=" lh-lg mb-4 text-justify">
-                                بورس انرژی یکی از بورس‌های کالایی است که به عنوان چهارمین بورس ایران، از سال ۱۳۹۱ فعالیت
-                                خود را شروع کرده است.
-                                این بازار در راستای خرید و فروش منابع انرژی، تامین مالی برای تولیدکنندگان، توزیع‌کنندگان
-                                و مصرف‌کنندگان،
-                                با هدف کشف قیمت روزانه حامل‌های انرژی و ایجاد شفافیت بیشتر در معاملات این گروه از
-                                محصولات شکل گرفته است.
-                            </p>
-                            <div className="p-4 rounded-3 fadak-bg-cream shadow-sm d-flex flex-column gap-4 justify-content-center align-items-center">
-                                <h5 className="fadak-text-navy">شفافیت و پویایی</h5>
-                                <p className="mb-0">ایجاد بستری امن برای معاملات نفت، گاز و برق</p>
-                            </div>
-                            <a href="#" className="custom-btn-navy mt-4 text-center float-end">
-                                <i className="bi bi-file-earmark-text-fill"></i> کدگیری و پذیرش
-                            </a>
-                        </div>
-                        <div className="col-lg-6 text-center">
-                            <img src="https://picsum.photos/600/400" className="metal-img rounded-4"/>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section className="vision-section">
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-md-8">
-                            <h4 className="vision-title">دریافت کد بورس انرژی برای اشخاص حقوقی</h4>
-                            <p className="lead mt-3">
-                                اولین و مهمترین گام جهت فعالیت در بورس انرژی دریافت »کد معامالتی« است. متقاضیان باید از طریق کارگزاری فدک
-                                و با تحویل مدارک و فرمهای مشخص شده، اقدام به دریافت کد معامالتی کنند.
-                            </p>
-                        </div>
-                        <div className="row justify-content-center align-items-center text-center py-2 rounded-2 mt-5" style={{backgroundColor: "rgb(0 61 106)"}}>
-
-                            <div className="col-md-4">
-                                <div className="contact-item text-white">
-                                    <a href="tel:02163479500" className="text-white text-decoration-none d-block gap-2 d-flex align-items-center justify-content-center">
-                                        <span>02163479500-501</span>
-                                        <i className="bi bi-telephone-fill"></i>
+                                    <a   href="https://t.me/KalaPlusMarket"
+                                         className="d-block mt-5 fadak-color-navy fadak-btn-outline"
+                                         target="_blank"
+                                         rel="noopener noreferrer">
+                                        لینک کانال تلگرامی
                                     </a>
+
                                 </div>
                             </div>
-
-                            <div className="col-md-4">
-                                <div className="contact-item text-white">
-                                    <a href="mailto:Commodity@fadakbrokerage.ir" className="text-white text-decoration-none d-block gap-2 d-flex align-items-center justify-content-center">
-                                        <span>Commodity@fadakbrokerage.ir</span>
-                                        <i className="bi bi-envelope-fill"></i>
-                                    </a>
-                                </div>
-                            </div>
-
                         </div>
+
                     </div>
-                </div>
-            </section>
 
 
-            <section className="py-5 fadak-bg-navy text-white">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-7">
-                            <h4 className="mb-4 text-white border-bottom border-warning d-inline-block pb-2">مدارک و فرم‌های لازم</h4>
-                            {/*<p className="mb-4 text-white">جهت تسریع در روند دریافت کد، لطفا مدارک زیر را آماده نمایید:</p>*/}
-
-                            <ul className="list-unstyled doc-list text-white">
-                                <li>تکمیل فرم احراز هویت مشتریان</li>
-                                <li>تکمیل فرم درخواست كد مشتریان</li>
-                                <li>تکمیل و امضای فرم بیانیه ریسک (مخصوص مشتریان بازار مشتقه برق)</li>
-                                <li>تصویر اساسنامه و ارائه شناسه ملی شرکت، پروانه بهره‌برداری، شرکت نامه</li>
-                                <li>کپی روزنامه رسمی آخرین تغییرات شرکت</li>
-                                <li>كپی شناسنامه و کارت ملی اعضای هیئت مدیره و مدیرعامل شركت (صاحبان امضای مجاز)</li>
-                                <li>ارائه مدارک مربوط به آخرین تغییرات مدیران و صاحبان امضا</li>
-                                <li>اعلام شماره حساب در قالب یک نامه جهت واریز وجه</li>
-                            </ul>
-                        </div>
-
-                        <div className="col-lg-5 mt-5 mt-lg-0">
-                            <div className="card h-100 bg-white border-0 shadow">
-                                <div className="card-body p-4 text-center d-flex flex-column justify-content-center">
-                                    <h4 className="fadak-text-navy mb-4">دانلود فرم‌ها</h4>
-
-                                    <div className="d-grid gap-3">
-                                        <a href="/assets/doc/IdentityVerification.pdf" className="btn fadak-button py-3">
-                                             دانلود فرم احراز هویت
-                                        </a>
-                                        <a href="#" className="btn custom-btn-navy py-3">
-                                             دانلود فرم درخواست کد
-                                        </a>
-                                    </div>
-
-                                    {/*<div className="mt-4 pt-4 border-top border-secondary">*/}
-                                    {/*    <p className="small text-muted mb-2">نیاز به راهنمایی دارید؟</p>*/}
-                                    {/*    <a href="https://wa.me/989120000000" className="btn btn-outline-success w-100">*/}
-                                    {/*        <i className="fab fa-whatsapp me-2"></i> ارسال مدارک در واتساپ*/}
-                                    {/*    </a>*/}
-                                    {/*</div>*/}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </section>
 
