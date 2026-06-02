b54e715d8926f1be1f9938e9433b0e17a8dca934
Amiri267 <m.amiri@gmail.com>
2026-06-02 13:50:44 +0330

    repair

--- src/section/SectionSupplyConsultant2/SectionSupplyConsultant2.tsx
+++ src/section/SectionSupplyConsultant2/SectionSupplyConsultant2.tsx
@@ -20,27 +20,62 @@ export default function SectionSupplyConsultant2() {
                             <p className="mt-3">تأمین مالی پایدار و کم‌هزینه، کلید توسعه و افزایش ظرفیت‌های عملیاتی بنگاه‌های اقتصادی با ورود به
                                 بازار سرمایه.</p>
 
-                            <div className="row justify-content-center align-items-center text-center my-3 py-2 rounded-2" style={{backgroundColor: "#003d6a"}}>
+                            {/*<div className="row justify-content-center align-items-center text-center my-3 py-2 rounded-2" style={{backgroundColor: "#003d6a"}}>*/}
 
-                                <div className="col-md-6">
-                                    <div className="contact-item text-white">
-                                        <a href="tel:02163479410" className="text-white text-decoration-none d-block gap-2 d-flex align-items-center justify-content-center">
-                                            <span className="number-farsi">02163479410</span>
-                                            <i className="bi bi-telephone-fill"></i>
-                                        </a>
+                            {/*    <div className="col-md-6">*/}
+                            {/*        <div className="contact-item text-white">*/}
+                            {/*            <a href="tel:02163479410" className="text-white text-decoration-none d-block gap-2 d-flex align-items-center justify-content-center">*/}
+                            {/*                <span className="number-farsi">02163479410</span>*/}
+                            {/*                <i className="bi bi-telephone-fill"></i>*/}
+                            {/*            </a>*/}
+                            {/*        </div>*/}
+                            {/*    </div>*/}
+
+                            {/*    <div className="col-md-6">*/}
+                            {/*        <div className="contact-item text-white">*/}
+                            {/*            <a href="mailto:marketentry@fadakbrokerage.ir" className="text-white text-decoration-none d-block gap-2 d-flex align-items-center justify-content-center">*/}
+                            {/*                <span>marketentry@fadakbrokerage.ir</span>*/}
+                            {/*                <i className="bi bi-envelope-fill"></i>*/}
+                            {/*            </a>*/}
+                            {/*        </div>*/}
+                            {/*    </div>*/}
+
+                            {/*</div>*/}
+
+
+
+
+
+                            <div className="container mt-5 px-0" dir="rtl">
+
+                                <div className="card border-0 shadow-sm rounded-4 p-4">
+                                    <div className="row align-items-center">
+
+                                        <div className="col-12 col-md-6 d-flex align-items-center">
+                                            <div>
+                                                <a  href="mailto://marketentry@fadakbrokerage.ir"
+                                                    className="fadak-color-navy text-decoration-none d-block d-flex align-items-center justify-content-center">
+                                                    <span dir="ltr">marketentry@fadakbrokerage.ir</span>
+                                                </a>
+                                            </div>
+                                        </div>
+
+                                        <div className="col-12 col-md-6 text-start mt-sm-0 mt-4">
+                                            <div className="contact-item border border-2 p-3 border-dark rounded-2">
+
+                                                <a href="tel:02163479410"
+                                                   className="text-black text-decoration-none d-block gap-2 d-flex align-items-center justify-content-center">
+                                                    <span className="number-farsi dir-ltr">02163479410</span>
+                                                    <i className="bi bi-telephone-fill"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+
                                     </div>
                                 </div>
-
-                                <div className="col-md-6">
-                                    <div className="contact-item text-white">
-                                        <a href="mailto:marketentry@fadakbrokerage.ir" className="text-white text-decoration-none d-block gap-2 d-flex align-items-center justify-content-center">
-                                            <span>marketentry@fadakbrokerage.ir</span>
-                                            <i className="bi bi-envelope-fill"></i>
-                                        </a>
-                                    </div>
-                                </div>
-
                             </div>
+
+
 
                         </div>
 
