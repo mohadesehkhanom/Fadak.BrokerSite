726aca23af0018bd0f3a5147a4efd66503ed713c
Amiri267 <m.amiri@gmail.com>
2026-06-03 22:11:42 +0330

    repair

--- src/section/SectionHero/SectionHero.tsx
+++ src/section/SectionHero/SectionHero.tsx
@@ -6,8 +6,8 @@ export default function SectionHero(){
         <section className="hero-section d-flex align-items-center p-5 p-lg-0">
             <div className="hero-content d-flex align-items-center justify-content-center w-100">
                 <div>
-                    <h1 className="hero-title">همراهی قابل اعتماد در دنیای سرمایه‌گذاری</h1>
-                    <p className="hero-subtitle">
+                    <h1 className="hero-title text-white">همراهی قابل اعتماد در دنیای سرمایه‌گذاری</h1>
+                    <p className="hero-subtitle text-white">
                         با بیش از سه دهه تجربه در بازار سرمایه، ما به شما کمک می‌کنیم تا به اهداف مالی خود دست
                         یابید
                     </p>
