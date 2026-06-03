433fa946d4c0d59ca5b65821d6b96668528bc807
Amiri267 <m.amiri@gmail.com>
2026-06-03 22:16:41 +0330

    repair

--- src/app/supply-consultant/page.tsx
+++ src/app/supply-consultant/page.tsx
@@ -16,7 +16,6 @@ export default function ServicesPage() {
     return (
         <>
             <SectionNavbarMenu/>
-
             <SectionSupplyConsultant/>
             <SectionOrganization/>
             <SectionFooter/>
