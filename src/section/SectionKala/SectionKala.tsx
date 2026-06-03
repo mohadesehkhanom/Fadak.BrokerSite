cb9cd4fdee0b59f45c477d3f3d98a68cf700d24b
Amiri267 <m.amiri@gmail.com>
2026-06-03 22:37:53 +0330

    repair

--- src/section/SectionKala/SectionKala.tsx
+++ src/section/SectionKala/SectionKala.tsx
@@ -20,7 +20,7 @@ export default function SectionKala() {
                     <div className="row align-items-center">
                         <div className="col-lg-5 text-center">
                             <img
-                                src="https://placehold.co/600x400"
+                                src="/assets/images/slider/kala_no_text_600x400.webp"
                                 className="metal-img rounded-4"/>
                         </div>
                         <div className="col-lg-7 mb-4 mb-lg-0 mt-5 mt-lg-0">
