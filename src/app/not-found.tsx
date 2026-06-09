736bd76f325d8934cc7bae1f67a123e1354dbcf8
Amiri267 <m.amiri@gmail.com>
2026-06-09 11:56:14 +0330

    add icon

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,19 @@
 import SectionNavbarMenu from "@/section/SectionHeader/SectionNavbarMenu";
+import {Metadata} from "next";
 
+
+export const metadata: Metadata = {
+    title: "کارگزاری فدک",
+    description: '',
+    icons: {
+        icon: [
+            { url: "/fav96.png", sizes: "96x96", type: "image/png" },
+        ],
+        apple: [
+            { url: "/fav192.png", sizes: "192x192", type: "image/png" },
+        ],
+    },
+};
 export default function NotFound() {
     return (
 
