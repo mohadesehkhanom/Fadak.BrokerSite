9a8d2542a40f7e8835cacf46a59ff951d9028d7e
Amiri267 <m.amiri@gmail.com>
2026-06-09 11:44:25 +0330

    add icon

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,6 +10,14 @@ import {Metadata} from "next";
 export const metadata: Metadata = {
     title: 'درباره کارگزاری فدک',
     description: '',
+    icons: {
+        icon: [
+            { url: "/fav96.png", sizes: "96x96", type: "image/png" },
+        ],
+        apple: [
+            { url: "/fav192.png", sizes: "192x192", type: "image/png" },
+        ],
+    },
 };
 
 
