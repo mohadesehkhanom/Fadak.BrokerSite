9c97e2ab2117bed4ccb14491b4c5f8a749f476ac
Amiri267 <m.amiri@gmail.com>
2026-06-09 11:47:48 +0330

    add icon

--- src/app/Credit/page.tsx
+++ src/app/Credit/page.tsx
@@ -7,6 +7,14 @@ import {Metadata} from "next";
 export const metadata: Metadata = {
     title: "اعتبار",
     description: '',
+    icons: {
+        icon: [
+            { url: "/fav96.png", sizes: "96x96", type: "image/png" },
+        ],
+        apple: [
+            { url: "/fav192.png", sizes: "192x192", type: "image/png" },
+        ],
+    },
 };
 
 export default function ServicesPage() {
