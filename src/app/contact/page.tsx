c1233492f1a6dff94db370cb7ea7eb4297b9f043
Amiri267 <m.amiri@gmail.com>
2026-06-09 11:47:58 +0330

    add icon

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,9 +2,25 @@ import SectionFooter from "@/section/SectionFooter/SectionFooter";
 import SectionOrganization from "@/section/SectionOrganization/SectionOrganization";
 import SectionNavbarMenu from "@/section/SectionHeader/SectionNavbarMenu";
 import SectionContactUs from "@/section/SectionContactUs/SectionContactUs";
+import {Metadata} from "next";
+
+
+
+export const metadata: Metadata = {
+    title: "تماس با ما",
+    description: '',
+    icons: {
+        icon: [
+            { url: "/fav96.png", sizes: "96x96", type: "image/png" },
+        ],
+        apple: [
+            { url: "/fav192.png", sizes: "192x192", type: "image/png" },
+        ],
+    },
+};
+
 
 export default function Page() {
-
 
     return (
         <>
