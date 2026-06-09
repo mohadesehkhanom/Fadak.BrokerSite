87e5a3600e5afba1210f0220cfd8f9e2b673ece8
Amiri267 <m.amiri@gmail.com>
2026-06-09 11:45:08 +0330

    add icon

--- src/app/Bourse/page.tsx
+++ src/app/Bourse/page.tsx
@@ -9,6 +9,14 @@ import SectionBourseTimeLine from "@/section/SectionBourseTimeLine/SectionBourse
 export const metadata: Metadata = {
     title: "بورس",
     description: '',
+    icons: {
+        icon: [
+            { url: "/fav96.png", sizes: "96x96", type: "image/png" },
+        ],
+        apple: [
+            { url: "/fav192.png", sizes: "192x192", type: "image/png" },
+        ],
+    },
 };
 
 export default function ServicesPage() {
