da01dedb865612e730c95d7256ea8f9890474eed
Amiri267 <m.amiri@gmail.com>
2026-06-09 11:49:24 +0330

    add icon

--- src/app/market-making/page.tsx
+++ src/app/market-making/page.tsx
@@ -8,6 +8,14 @@ import {Metadata} from "next";
 export const metadata: Metadata = {
     title: "بازار گردانی",
     description: '',
+    icons: {
+        icon: [
+            { url: "/fav96.png", sizes: "96x96", type: "image/png" },
+        ],
+        apple: [
+            { url: "/fav192.png", sizes: "192x192", type: "image/png" },
+        ],
+    },
 };
 
 export default function Page() {
