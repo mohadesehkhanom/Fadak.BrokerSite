30fced9cb708ace7954604e886f98ef8f0811e01
Amiri267 <m.amiri@gmail.com>
2026-07-24 14:11:05 +0330

    change server config

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,15 +4,7 @@ import {Metadata} from "next";
 
 export const metadata: Metadata = {
     title: "کارگزاری فدک",
-    description: '',
-    icons: {
-        icon: [
-            { url: "/fav96.png", sizes: "96x96", type: "image/png" },
-        ],
-        apple: [
-            { url: "/fav192.png", sizes: "192x192", type: "image/png" },
-        ],
-    },
+    description: ''
 };
 export default function NotFound() {
     return (
